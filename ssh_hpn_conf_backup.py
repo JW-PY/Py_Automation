78e2585e2120b4cabed887cf951d8fdca189c767
TheBlobfish <woodze@internode.on.net>
2016-04-03 09:41:18 +0930

    Update ssh_hpn_conf_backup.py

--- ssh_hpn_conf_backup.py
+++ ssh_hpn_conf_backup.py
@@ -1,3 +1,4 @@
+
 import paramiko
 import time
 import os, datetime
@@ -29,49 +30,60 @@ def make_directory():
     #Create the directory for the output files dd/mm/yyyy
     mydir = os.path.join(os.getcwd(), datetime.datetime.now().strftime('%Y-%m-%d_%H-%M-%S'))
     os.makedirs(mydir)
+    os.chdir(mydir)
 
 
 if __name__ == '__main__':
 
-
-    # Call the make directory funtion
-    make_directory()
-
+    # List of divices to iterate over
+    HOST = {
+    '192.168.56.10': 'switch1',
+    '192.168.56.20': 'switch2'
+    }
     # VARIABLES THAT NEED CHANGING
     ip = '192.168.56.20'
     username = 'admin'
     password = 'public'
 
-    # Create instance of SSHClient object
-    remote_conn_pre = paramiko.SSHClient()
+    # Call the make directory funtion
+    make_directory()
 
-    # Automatically add untrusted hosts (make sure okay for security policy in your environment)
-    remote_conn_pre.set_missing_host_key_policy(
-         paramiko.AutoAddPolicy())
 
-    # initiate SSH connection
-    remote_conn_pre.connect(ip, username=username, password=password, look_for_keys=False, allow_agent=False)
-    print "SSH connection established to %s" % ip
+    for i in HOST:
+        # Create instance of SSHClient object
+        remote_conn_pre = paramiko.SSHClient()
 
-    # Use invoke_shell to establish an 'interactive session'
-    remote_conn = remote_conn_pre.invoke_shell()
-    print "Interactive SSH session established"
+        # Automatically add untrusted hosts (make sure okay for security policy in your environment)
+        remote_conn_pre.set_missing_host_key_policy(paramiko.AutoAddPolicy())
 
-    # Strip the initial router prompt
-    output = remote_conn.recv(1000)
+        # initiate SSH connection
+        print "Attempting connection to %s" % i
+        remote_conn_pre.connect(i, username=username, password=password, look_for_keys=False, allow_agent=False)
+        print "SSH connection established to %s" % i
 
-    # See what we have
-    print output
+        # Use invoke_shell to establish an 'interactive session'
+        remote_conn = remote_conn_pre.invoke_shell()
+        print "Interactive SSH session established"
 
-    # Turn off paging
-    disable_paging_hpn(remote_conn)
+        # Strip the initial router prompt
+        output = remote_conn.recv(1000)
 
-    # Now let's try to send the router a command
-    remote_conn.send("\n")
-    remote_conn.send("display interface brief\n")
-    time.sleep(2)
-    remote_conn.send("display cur\n")
-    time.sleep(3)    # Wait for the command to complete
+        # See what we have
+        print output
 
-    output = remote_conn.recv(5000)
-    print output
+        # Turn off paging by calling paging function
+        disable_paging_hpn(remote_conn)
+
+        # Now let's try to send the router a command
+        remote_conn.send("\n")
+        remote_conn.send("display interface brief\n")
+        time.sleep(2)
+        remote_conn.send("display cur\n")
+        time.sleep(3)    # Wait for the command to complete
+        output = remote_conn.recv(5000)
+        print output
+        #Write to a file with the name of the switch
+        f = open('%s.txt' %HOST[i], 'w')
+        f.write (output.decode('ascii'))
+        f.close()
+
